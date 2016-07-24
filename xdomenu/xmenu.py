b69310c9c6ee2a7545eeb53d28b07f2e882f856c
Nate Yonkee <tulanthoar@gmail.com>
2016-07-23 21:40:53 -0600

    expand capabilities

--- xdomenu/xmenu.py
+++ xdomenu/xmenu.py
@@ -1,23 +1,42 @@
 '''menu to interface with xmonad'''
+from time import sleep
 from click import echo, getchar, command
 from sh import Command
+from ewmh import EWMH
 
 
-@command()
-def xdomenu():
-    """Shows a simple menu."""
-    char_to_bin = {'q': ('xmctl', 'srmenu'),
-                   'j': ('xmctl', 'jmenu'),
-                   'n': ('xmctl', 'nvim'),
-                   'h': ('urxvt', ['-e', 'htop']),
-                   'u': ('urxvt', ['-name', 'urxvt', '-n', 'urxvt']),
-                   'i': ('xmctl', 'ipython'),
-                   'p': ('xmctl', 'perl'),
-                   'r': ('xmctl', 'ranger'),
-                   'a': ('xmctl', 'allpads'),
-                   'b': ('xmctl', 'byobu'),
-                   'P': ('xmctl', 'pomodoro')}
-    echo('Main menu:')
+def active_class(ewmh_obj):
+    '''returns the focused window class after 200ms'''
+    sleep(0.3)
+    win = ewmh_obj.getActiveWindow()
+    win_class = win.get_wm_class()
+    return win_class
+
+
+def class_is_mapped(hinter, class_name):
+    '''use ewmh to see if a window with class name is mapped'''
+    for win in hinter.getClientListStacking():
+        classnames = win.get_wm_class()
+        if class_name in classnames:
+            return True
+    return False
+
+
+def class_is_visible(hinter, class_name):
+    '''check if a window with class_name is currently visible'''
+    for win in hinter.getClientListStacking():
+        if class_name in win.get_wm_class():
+            attrs = win.get_attributes()
+            if attrs._data['map_state'] == 2:
+                return True
+    return False
+
+
+def print_menu(persist):
+    '''print menu of available keys'''
+    echo('Spc: next empty ws')
+    echo('Tab: toggle persist')
+    echo('  c: clipmenu')
     echo('  q: qutebrowser')
     echo('  j: j4-app-menu')
     echo('  n: neovim')
@@ -29,6 +48,67 @@ def xdomenu():
     echo('  P: pomodoro')
     echo('  h: htop')
     echo('  u: urxvt')
-    char = getchar()
-    cmd = Command(char_to_bin[char][0])
-    cmd(char_to_bin[char][1])
+    if persist:
+        echo('Persistence on')
+    else:
+        echo('Persistence off')
+
+
+@command()
+def xdomenu():
+    """interacts with a simple menu."""
+    xmc = Command('xmctl')
+    urx = Command('urxvt')
+    clip = Command('clipmenu')
+    char_to_bin = {'q': (xmc, 'srmenu'),
+                   'c': (clip, "-z -w 800 -l 50 -p clip".split()),
+                   'j': (xmc, 'jmenu'),
+                   'n': (xmc, 'nvim'),
+                   'h': (urx, ['-e', 'htop']),
+                   'u': (urx, ['-name', 'urxvt', '-n', 'urxvt']),
+                   'i': (xmc, 'ipython'),
+                   'p': (xmc, 'perl'),
+                   'r': (xmc, 'ranger'),
+                   'a': (xmc, 'allpads'),
+                   'b': (xmc, 'byobu'),
+                   'P': (xmc, 'pomodoro')}
+    xdo = Command('xdotool')
+    hinter = EWMH()
+    persistent = False
+    print_menu(persistent)
+    while True:
+        char = getchar()
+        if char == '\x1b':
+            raise KeyboardInterrupt
+        if char == '\t':
+            if not persistent:
+                persistent = True
+                echo('Persistence on')
+            else:
+                persistent = False
+                echo('Persistence off')
+            continue
+        if char == ' ':
+            xdo(['key', 'Menu'])
+            xmc('nextempty')
+            xdo(['key', 'Menu'])
+            continue
+        xdo(['key', 'Menu'])
+        if char == 'b':
+            if not class_is_mapped(hinter, 'urxv'):
+                xmc('byobu')
+                sleep(1)
+            else:
+                if class_is_visible(hinter, 'urxv'):
+                    xmc('sendbyo')
+                else:
+                    xmc('bringbyo')
+        else:
+            try:
+                (cmd, opts) = char_to_bin[char]
+                cmd(opts)
+            except KeyError:
+                echo('key ' + char + ' not recognized')
+        if persistent:
+            sleep(0.1)
+            xdo(['key', 'Menu'])
